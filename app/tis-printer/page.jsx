e2f73507bb9d02fa47858782cc0edf9c7f45cc2e
jirawut-TIS <jirawut@totalitsolution.co.th>
2026-03-19 16:39:01 +0700

    Delete app directory

--- app/tis-printer/page.jsx
+++
@@ -1,3 +0,0 @@
-import PrinterClient from './PrinterClient';
-export const metadata = { title: 'TIS Service', description: 'Printer PDF to Excel' };
-export default function TISPrinterPage() { return <PrinterClient />; }
